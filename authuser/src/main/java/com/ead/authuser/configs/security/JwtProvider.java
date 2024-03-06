af02f8ca998bc106543f07ee534bbead185c6824
Márcio Monteiro <tsp.marcio.monteiro@sympla.com.br>
2024-03-06 01:37:31 -0300

    Semana 7: Authentication e Authorization com Json Web Token (JWT)

---
+++ authuser/src/main/java/com/ead/authuser/configs/security/JwtProvider.java
@@ -0,0 +1,57 @@
+package com.ead.authuser.configs.security;
+
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@Component
+public class JwtProvider {
+
+	@Value("${ead.auth.jwtSecret}")
+	private String jwtSecret;
+	@Value("${ead.auth.jwtExpirationMs}")
+	private int jwtExpirationMs;
+	
+	public String generateJwt(Authentication authentication) {
+		UserDetailsImpl userPrincipal = (UserDetailsImpl)authentication.getPrincipal();
+		return Jwts.builder()
+					.setSubject(userPrincipal.getUsername())
+					.setIssuedAt(new Date())
+					.setExpiration(new Date( (new Date()).getTime() + jwtExpirationMs ))
+					.signWith(SignatureAlgorithm.HS512, jwtSecret)
+					.compact();
+	}
+	
+	public String getUsernameJwt(String token) {
+		return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+	}
+	
+	public boolean validadeJwt(String authToken) {
+		try {
+			Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+			return true;
+		} catch(SignatureException e) {
+			log.error("Invalid Jwt signature: {}", e.getMessage());
+		} catch(MalformedJwtException e) {
+			log.error("Invalid Jwt token: {}", e.getMessage());
+		} catch(ExpiredJwtException e) {
+			log.error("Jwt token is expired: {}", e.getMessage());
+		} catch(UnsupportedJwtException e) {
+			log.error("Jwt token is unsupported: {}", e.getMessage());
+		} catch(IllegalArgumentException e) {
+			log.error("Jwt claims string is empty: {}", e.getMessage());
+		}
+		return false;
+	}
+}
